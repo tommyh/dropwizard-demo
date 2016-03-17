0e561e3a1568da1a2463071b3b327a63942e68fc
Tom Hallett <tomhallett@gmail.com>
2016-03-17 01:10:24 -0700

    trying a new run script

--- run.sh
+++ run.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-java -jar target/dropwizard-demo-0.0.1-SNAPSHOT.jar server sample.yml
+curl -o /tmp/dropwizard-demo-0.0.1-SNAPSHOT.jar https://github.com/tommyh/dropwizard-demo/blob/master/target/dropwizard-demo-0.0.1-SNAPSHOT.jar?raw=true
+curl -o /tmp/sample.yml https://raw.githubusercontent.com/tommyh/dropwizard-demo/master/sample.yml
+java -jar /tmp/dropwizard-demo-0.0.1-SNAPSHOT.jar server /tmp/sample.yml
