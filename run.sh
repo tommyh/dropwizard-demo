873a4999af640c57ec1d7d901436b69e522a0f91
Tom Hallett <tomhallett@gmail.com>
2016-03-17 00:47:27 -0700

    sikuli hello world

--- run.sh
+++ run.sh
@@ -1,1 +1,2 @@
+#!/bin/bash
 java -jar target/dropwizard-demo-0.0.1-SNAPSHOT.jar server sample.yml
--- src/main/java/com/yammer/dropwizarddemo/resources/HelloWorldResource.java
+++ src/main/java/com/yammer/dropwizarddemo/resources/HelloWorldResource.java
@@ -3,11 +3,29 @@ package com.yammer.dropwizarddemo.resources;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
+import javax.ws.rs.QueryParam;
 
-@Path("/greetings/{name}")
+import org.sikuli.script.*;
+
+// TODO: use reflection for java classes (and methods)
+//import java.lang.reflect.Constructor;
+
+
+@Path("/cmd/screen/")
 public class HelloWorldResource {
-    @GET
-    public String getGreeting(@PathParam("name") String name){
-        return "Hello, " + name + "!";
+
+    @GET @Path("/click")
+    public String click(@QueryParam("x") Integer x, @QueryParam("y") Integer y) throws FindFailed {
+        Screen s = new Screen();
+        Location loc = new Location(x,y);
+        s.click(loc);
+        return "success";
+    }
+
+    @GET @Path("/write")
+    public String write(@QueryParam("str") String str) {
+        Screen s = new Screen();
+        s.write(str);
+        return "success";
     }
 }
